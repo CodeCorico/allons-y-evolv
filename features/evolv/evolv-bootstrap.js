f82ba0139604a43f7a61878e8ddd1cb35b1869bb
Xavier Boubert <xavier@boubert.fr>
2016-08-26 15:06:21 +0200

    feat(evolv): create the feature

---
+++ features/evolv/evolv-bootstrap.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = function($allonsy, $options, $done) {
+  if ((process.env.EVOLV && process.env.EVOLV == 'false') || (!$options.owner || $options.owner != 'start')) {
+    return $done();
+  }
+
+  var evolv = require('evolv'),
+      async = require('async'),
+      path = require('path'),
+      fs = require('fs-extra'),
+      versionsPath = path.resolve('.versions'),
+      versions = {};
+
+  (fs.existsSync(versionsPath) ? fs.readFileSync(versionsPath, 'utf-8') : '').split('\n').map(function(version) {
+    version = version.split('=');
+    if (version.length < 2 || !version[0].trim() || !version[1].trim()) {
+      return;
+    }
+
+    versions[version[0].trim()] = version[1].trim();
+  });
+
+  var pathsToEvolv = [{
+    module: 'this',
+    path: path.resolve('.')
+  }];
+
+  fs.readdirSync(path.resolve('./node_modules/')).forEach(function(folder) {
+    if (folder.indexOf('allons-y-') < 0) {
+      return;
+    }
+
+    pathsToEvolv.push({
+      module: folder,
+      path: path.resolve('./node_modules/' + folder)
+    });
+  });
+
+  async.eachSeries(pathsToEvolv, function(pathToEvolv, nextPath) {
+    var packageFile = path.join(pathToEvolv.path, 'package.json'),
+        moduleVersionsPath = path.resolve(pathToEvolv.path, 'versions');
+
+    if (!fs.existsSync(packageFile)) {
+      return nextPath();
+    }
+
+    var packageJson = fs.readJsonSync(packageFile);
+
+    if (!packageJson || !packageJson.version) {
+      return nextPath();
+    }
+
+    versions[pathToEvolv.module] = versions[pathToEvolv.module] || packageJson.version;
+
+    if (versions[pathToEvolv.module] == packageJson.version || !fs.existsSync(moduleVersionsPath)) {
+      return nextPath();
+    }
+
+    $allonsy.outputInfo('► [Evolve] "' + pathToEvolv.module + '" to ' + packageJson.version + '...');
+
+    evolv({
+      path: moduleVersionsPath,
+      silent: true
+    }, versions[pathToEvolv.module], function() {
+      $allonsy.outputSuccess(' DONE\n');
+
+      versions[pathToEvolv.module] = packageJson.version;
+
+      nextPath();
+    });
+
+  }, function() {
+    fs.writeFileSync(versionsPath, Object
+      .keys(versions)
+      .map(function(key) {
+        return key + '=' + versions[key];
+      })
+      .join('\n')
+    );
+
+    $done();
+  });
+};
